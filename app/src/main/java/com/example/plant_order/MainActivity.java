e094cbc1ecb538cb8aaac9baec25bf429effb18f
Darren Ho <darrenhoshuyi@gmail.com>
2020-03-11 00:14:23 +0800

    Merge branch 'master' of https://github.com/hoshuyi/Order
    # Conflicts:
    #	app/src/main/java/com/example/plant_order/MainActivity.java

--- app/src/main/java/com/example/plant_order/MainActivity.java
+++ app/src/main/java/com/example/plant_order/MainActivity.java
@@ -5,7 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
     private static int TIME_OUT = 3000;
