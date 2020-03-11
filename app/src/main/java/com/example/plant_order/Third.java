a67edbcee565cfc1bf90ac95ff911ae939e5facc
Galaxysternmond <gonniesternmond@gmail.com>
2020-03-11 22:16:51 +0800

    Third sync

--- app/src/main/java/com/example/plant_order/Third.java
+++ app/src/main/java/com/example/plant_order/Third.java
@@ -6,11 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 
 public class Third extends AppCompatActivity {
-
+    private Intent secondIntent = getIntent();
+    private String string_subtotal = secondIntent.getStringExtra (Second.EXTRA_MESSAGE);
+    private int int_subtotal = Integer.parseInt(string_subtotal);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_third);
-        Intent secondIntent = getIntent();
     }
 }
