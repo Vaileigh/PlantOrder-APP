935875a5bf37cfba8a207ff49872543272b41482
Galaxysternmond <gonniesternmond@gmail.com>
2020-03-12 21:49:39 +0800

    Complete validate

--- app/src/main/java/com/example/plant_order/Third.java
+++ app/src/main/java/com/example/plant_order/Third.java
@@ -4,10 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -137,11 +139,71 @@ public class Third extends AppCompatActivity implements
             shippingSubtotal(spinnerLabel);
             totalPayment();
             displayToast(spinnerLabel);
+
         }
 
         @Override
         public void onNothingSelected(AdapterView<?> parent) {
 
         }
+
+    public void placeOrder(View view) {
+        validate();
     }
+    public void validate(){
+        EditText name,phone,state,area,postalCode,detailedAddr;
+        name = findViewById(R.id.enterName);
+        String enter_name = name.getText().toString();
+        phone = findViewById(R.id.enterPhone);
+        String enter_phone = phone.getText().toString();
+        state = findViewById(R.id.enterState);
+        String enter_state = state.getText().toString();
+        area = findViewById(R.id.enterArea);
+        String enter_area = area.getText().toString();
+        postalCode = findViewById(R.id.enterPostalCode);
+        String enter_postal = postalCode.getText().toString();
+        detailedAddr = findViewById(R.id.editDetailedAddress);
+        String enter_addr = detailedAddr.getText().toString();
+
+        TextView validate_name,validate_phone,validate_state,validate_area, validate_postal, validate_addr;
+        validate_name=findViewById(R.id.textView_ValidateName);
+        validate_phone=findViewById(R.id.textView_ValidateName2);
+        validate_state=findViewById(R.id.textView_ValidateName3);
+        validate_area=findViewById(R.id.textView_ValidateName4);
+        validate_postal=findViewById(R.id.textView_ValidateName5);
+        validate_addr=findViewById(R.id.textView_ValidateName6);
+
+        if(TextUtils.isEmpty(enter_name)){
+            validate_name.setVisibility(View.VISIBLE);
+        }
+        else
+            validate_name.setVisibility(View.INVISIBLE);
+
+        if(TextUtils.isEmpty(enter_phone)){
+            validate_phone.setVisibility(View.VISIBLE);
+        }
+        else
+            validate_phone.setVisibility(View.INVISIBLE);
+        if(TextUtils.isEmpty(enter_state)){
+            validate_state.setVisibility(View.VISIBLE);
+        }
+        else
+            validate_state.setVisibility(View.INVISIBLE);
+        if(TextUtils.isEmpty(enter_area)){
+            validate_area.setVisibility(View.VISIBLE);
+        }
+        else
+            validate_area.setVisibility(View.INVISIBLE);
+        if(TextUtils.isEmpty(enter_postal)){
+            validate_postal.setVisibility(View.VISIBLE);
+        }
+        else
+            validate_postal.setVisibility(View.INVISIBLE);
+        if(TextUtils.isEmpty(enter_addr)){
+            validate_addr.setVisibility(View.VISIBLE);
+        }
+        else
+            validate_addr.setVisibility(View.INVISIBLE);
+    }
+}
 
